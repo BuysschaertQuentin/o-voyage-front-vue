55bb781df0678406a3fa44f2d0d389caf6910993
Your NameBuysschaertQuentin <quentin.buysschaert@gmail.com>
2023-08-07 23:33:48 +0200

    feat: deploy

--- src/components/widgets/CountryWidget.vue
+++ src/components/widgets/CountryWidget.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import countries from '@/datas/countries.json'
 
 const props = defineProps({
   name: {
@@ -11,8 +12,10 @@ const props = defineProps({
 const alt = computed(() => `Drapeau de ${props.name}`)
 const image = ref()
 const url = `https://o-voyage-en-vue-9gnia.ondigitalocean.app/svg`
+const country = countries.find((country) => country.name === props.name)
 
-image.value = `${url}/${props.name.toLowerCase()}.svg`
+if(country)
+image.value = `${url}/${country.code.toLowerCase()}.svg`
 
 </script>
 
